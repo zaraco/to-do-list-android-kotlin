5f2e6fbdecb050f5083f5a512875a17f38b96782
Zaraco <zahrateymouri90@gmail.com>
2020-08-05 16:33:52 +0200

    task repository added to activity

--- app/src/main/java/com/zaraco/todolist/MainActivity.kt
+++ app/src/main/java/com/zaraco/todolist/MainActivity.kt
@@ -2,10 +2,21 @@ package com.zaraco.todolist
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.zaraco.todolist.managers.TaskRepository
+import com.zaraco.todolist.models.Task
 
 class MainActivity : AppCompatActivity() {
+
+    val taskRepository = TaskRepository()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun addTask(text: String) {
+        val task = Task()
+        task.text = text
+        taskRepository.add(task)
     }
 }
--- app/src/main/java/com/zaraco/todolist/managers/TaskRepository.kt
+++ app/src/main/java/com/zaraco/todolist/managers/TaskRepository.kt
@@ -4,7 +4,7 @@ import com.zaraco.todolist.models.Task
 
 class TaskRepository {
 
-    var tasks: MutableList<Task> = ArrayList()
+    val tasks: MutableList<Task> = ArrayList()
 
     fun add(task: Task) {
         tasks.add(task)
