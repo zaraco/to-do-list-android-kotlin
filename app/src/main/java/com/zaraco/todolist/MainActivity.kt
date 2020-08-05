d33048d54acb3a4b71a3e709b3def7a6bf158e08
Zaraco <zahrateymouri90@gmail.com>
2020-08-05 16:58:49 +0200

    click listener and recyclerview added

--- app/src/main/java/com/zaraco/todolist/MainActivity.kt
+++ app/src/main/java/com/zaraco/todolist/MainActivity.kt
@@ -2,21 +2,27 @@ package com.zaraco.todolist
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import com.zaraco.todolist.managers.TaskRepository
 import com.zaraco.todolist.models.Task
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
-    val taskRepository = TaskRepository()
+    private val taskRepository = TaskRepository()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        addBtn?.setOnClickListener {
+            addTask(taskEt?.text?.toString())
+        }
     }
 
-    fun addTask(text: String) {
+    private fun addTask(text: String?) {
         val task = Task()
         task.text = text
         taskRepository.add(task)
+        Log.i("task", text ?: "")
     }
 }
